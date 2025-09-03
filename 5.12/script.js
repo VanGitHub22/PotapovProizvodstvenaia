6afb95b4cf83fcb26d0d48b1caec4adfe7939908
vanmik <vanmikyt@mail.ru>
2025-09-03 15:29:50 +0500

    5.12

---
+++ 5.12/script.js
@@ -0,0 +1,29 @@
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+let jsonUser = JSON.stringify(user)
+console.log(jsonUser)
+jsonUser = JSON.parse(jsonUser)
+console.log(jsonUser)
+
+
+
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+  return (key != "" && value == meetup) ? undefined : value;
+}));
