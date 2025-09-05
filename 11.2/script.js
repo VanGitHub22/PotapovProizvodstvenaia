d49813342d83f76a1a7f553d89e6ac0f4ba6e772
vanmik <vanmikyt@mail.ru>
2025-09-05 14:51:07 +0500

    11.2

---
+++ 11.2/script.js
@@ -0,0 +1,19 @@
+/*let promise = new Promise(function(resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(alert); //1, второй resolve будет проигнорирован
+
+
+
+
+
+function delay(ms) {
+   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+delay(3000).then(() => alert('выполнилось через 3 секунды'));*/
+
+
