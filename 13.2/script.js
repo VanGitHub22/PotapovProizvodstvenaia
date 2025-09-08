4bd57af01bde2cd78be47f43d55ac891d9ee991e
vanmik <vanmikyt@mail.ru>
2025-09-08 12:28:02 +0500

    13.2

---
+++ 13.2/script.js
@@ -0,0 +1,7 @@
+import {sayHi, sayBye} from './say.js';
+import {sayHi as hi, sayBye as bye} from './say.js';
+
+
+
+export let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+export {sayHi as hi, sayBye as bye};
