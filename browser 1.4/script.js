1a550259e6dce1caf76dad4dbee7112b600d8017
vanmik <vanmikyt@mail.ru>
2025-09-08 15:18:37 +0500

    browser 1.4

---
+++ browser 1.4/script.js
@@ -0,0 +1,7 @@
+let table = document.querySelector("#age-table")
+let labels = table.querySelectorAll("label")
+let td = table.rows[0].cells[0]
+let form = document.getElementsByName("search")[0]
+let firstInput = form.getElementsByTagName("input")[0]
+let inputs = form.querySelectorAll("input")
+let lastInput = inputs[inputs.length-1]
